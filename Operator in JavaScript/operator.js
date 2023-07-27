68af8308bc1f69264963114db49537dca45f268e
Niraj Kumar <niranjan888k@gmail.com>
2023-07-27 12:55:04 +0530

    Add files via upload

---
+++ Operator in JavaScript/operator.js
@@ -0,0 +1,187 @@
+// Addition
+const firstnumber=23;
+const secondnumber=45;
+const result=firstnumber+secondnumber;
+console.log(result )
+
+// subtraction
+let num1 = 34;
+let num2 = 23;
+
+const subtraction=num1-num2;
+console.log(subtraction)
+
+// multiplication
+
+let d=45;
+let e=23;
+ const multiple = d*e;
+ console.log(multiple);
+
+//  division
+const q=12;
+const p=12;
+
+division=q/p;
+console.log(division);
+
+//  Remainder
+
+let r=15;
+let s=7;
+
+let result3=r%s
+console.log(result3);
+
+//increment
+
+let x=12;
+x++;    // x=x-1 you can write like this also
+console.log(x);
+
+// decreament 
+let ad=12;
+ad--          // ad=ad-1;
+console.log(ad);
+
+ // exponential
+ let l=2;
+ let k=10;
+ const result4=l**k;
+ console.log(result4);
+
+
+// <<<< Arithmetic operator the end >>>
+
+
+// <<<< Assignment operator started >>>>
+
+// Assignment operator
+let thirdnumber1=4;
+
+console.log(thirdnumber1)
+
+// Additin Assignment
+let thirdnumber=4;
+thirdnumber+=4; 
+console.log(thirdnumber)
+
+// subtraction Assignment
+
+let niraj=14;
+niraj-=2;                       //niraj = niraj-2
+console.log(niraj);
+
+// multiplication Assignment
+
+let priyanshu=85;
+priyanshu*=2;               //priyanshu = priyanshu*2
+console.log(priyanshu);
+
+// Divisionn Assignment
+let priti=12;
+priti/=2                    // priti = priti/2
+console.log(priti)
+
+// Remainder Assignment
+
+let faimly=13;
+faimly%=2;                  //faimly = faimly%2
+console.log(faimly)
+
+// Exponential Assignment
+let n = 4;
+n**=2;
+console.log(n);
+
+// <<< Assignment operator the end >>>>
+
+// comparison operators
+
+// Equal to 
+ let abhi=22;
+ let bhi=22;
+ niraj= abhi==bhi;
+ console.log(niraj)
+
+//  not equal to
+
+let ramu=12;
+let v=12;
+servant= ramu!=v;
+console.log(servant);
+
+//  strict equal to
+
+let h=12;
+let j=4;
+k=h===j;
+console.log(k);
+
+// strict not equal
+
+let abhii=22;
+ let bhii=22;
+ Niraaj= abhii!==bhii;
+ console.log(Niraaj)
+
+// greaterthan
+
+let w=13;
+let z=12;
+let m=w>z;
+console.log(m);
+
+// greaterthan or equal tio
+
+let nk=23;
+let kn=232;
+let nkk= nk>=kn;
+console.log(nkk);
+
+// leasthan 
+
+let bk=12;
+let kb=42;
+let b = bk<kb;
+console.log(b);
+
+// lessthan equal to 
+let kh=12;
+let hk=11;
+let hj= kh<=hk;
+console.log(hj);
+
+// <<< comparison operators the end >>>>
+
+// <<<< logical operator >>>>
+
+// Logtical AND 
+
+let ac =true;
+let bc=false;
+let cd = ac && bc       // true && false become = false -- 0
+                        // true && true become = true --- 1
+                        // false && false become =  false --- 0
+                        // false && true become = false -- 0
+console.log(cd);
+
+// logical or
+let bn=true;
+let nb= false;
+let mn= bn || nb;          // true && false become = true -- 1
+                          // true && true become = true --- 1
+                         // false && false become =  false --- 0
+                        // false && true become = true -- 1
+
+console.log(mn);
+
+//  Logical Not operator
+
+let u=true;
+let ikk = false;
+console.log(!u); 
+                        // if there is true then it's become false--0
+                        // if there is false then it's become true--1
+
+// <<<< Logical operator the end >>>>
