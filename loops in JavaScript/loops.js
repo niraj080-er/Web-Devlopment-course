0bc44e161cbd8560167ea162cec12c953f787258
Niraj Kumar <niranjan888k@gmail.com>
2023-07-30 14:54:09 +0530

    Add files via upload

---
+++ loops in JavaScript/loops.js
@@ -0,0 +1,35 @@
+// for loop 
+for(int=0; int<5; int++){
+    console.log("", int);
+}
+
+//  while loop
+
+ let step=0;
+ while(step>5){
+
+    console.log('step', step);
+         sstep += 1;
+  }
+
+//   do-while loop
+
+let step1 = 0;
+do{
+    console.log("step1" ,step1);
+    step1 +=1;
+} 
+    while (step1< 5);
+
+//  continue and break statement
+
+ 
+ let step2 = 0;
+ while(step2 > 5 ){
+    console.log('step2', step2);
+    step2 += 1;
+    if(step2 ===2) {
+       continue;
+    }
+ }
+   
