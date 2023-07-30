0141bbc1fa03a75c909366566f07405f6ec1dfdb
Niraj Kumar <niranjan888k@gmail.com>
2023-07-30 14:51:32 +0530

    Add files via upload

---
+++ switch-case  statement/switchcase.js
@@ -0,0 +1,22 @@
+const grade="g"
+
+switch(grade){
+    case "A":
+        console.log("A --> Very good");
+    break;
+    case "B":
+        console.log("B --> Mark is good keep learning");
+        break;
+    case "C":
+        console.log("C--> Low marks you have to improve");
+        break;
+    case "D":
+        console.log("D--> Mark is very low you have to do hard work");
+    break;
+    case "E":
+        console.log("E--> Mark is very very low you have to hard work");
+    break;
+    default:
+        console.log("Better luck next time");
+
+}
