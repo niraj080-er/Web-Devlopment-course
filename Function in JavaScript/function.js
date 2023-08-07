1d32032c6d8981af8f034d1bdbfa132f082a7988
Niraj Kumar <niranjan888k@gmail.com>
2023-08-07 15:11:58 +0530

    Add files via upload

---
+++ Function in JavaScript/function.js
@@ -0,0 +1,28 @@
+function kushwaha(a, b){                // Here function name is Kushwaha
+    const sum=a+b;                      // operation are happining through the addition 
+    console.log("result is", sum)       // the output will show the result is ........(sum of the nuber)
+    return sum;                         // returning the sum 
+}
+const a=12;
+const b=13;
+const result = kushwaha(a,b);
+console.log("return result" ,result);           // using return of the sum that is mention in line number 4 ---> return sum;
+
+const  c=120;
+const d=120;
+kushwaha(c,d);
+
+const niraj=12;
+const kumar=50;
+kushwaha(niraj,kumar);
+    
+// expression function
+const square=function(num){             // without decalaring the function you can not  use expression function  but in the normal function you can execute the first and you can write the function after writing the code it will excute
+    return num*num;
+
+};
+ console.log(square(2))
+
+ 
+
+
---
+++ Function in JavaScript/nested.js
@@ -0,0 +1,9 @@
+function addsquare (a, b){
+    function square(num){
+        return num*num;
+    }
+     return square(a)+square(b);
+   
+}
+console.log (addsquare(2,3));
+console.log(addsquare(4,5));
